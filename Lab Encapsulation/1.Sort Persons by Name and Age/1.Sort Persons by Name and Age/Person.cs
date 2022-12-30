0a5e50f0c599bafed6f9a318f136ca58dc6e000a
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-12-30 21:54:02 +0200

    Add files via upload

---
+++ Lab Encapsulation/1.Sort Persons by Name and Age/1.Sort Persons by Name and Age/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+		public Person(string firstName, string lastName, int age, decimal salary)
+		{
+		   FirstName = firstName;
+		   LastName	= lastName;
+		   Age = age;
+           Salary = salary;
+
+        }
+		private string firstName;
+
+		public string FirstName
+        {
+			get { return firstName; }
+			private set 
+			{
+				if (value.Length >= 3)
+				{
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+            }
+		}
+		private string lastName;
+
+		public string LastName
+        {
+			get { return lastName; }
+            private set
+            {
+                if (value.Length >= 3)
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+		private int age;
+
+		public int Age
+        {
+			get { return age; }
+            private set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+		private decimal salary;
+
+		public  decimal Salary
+        {
+			get { return salary; }
+            private set
+            {
+                if (value >= 460)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+            }
+        }
+		public void IncreaseSalary(decimal percentage)
+		{
+			if (this.Age > 30)
+			{
+                this.Salary += this.Salary * percentage / 100;
+                
+            }
+			else
+			{
+                this.Salary += this.Salary * percentage / 200;
+            }
+		}
+
+        public override string ToString()
+		{
+			return $"{this.FirstName} {this.LastName} receives {Salary:f2} leva.";
+		}
+
+	}
+}
---
+++ Lab Encapsulation/1.Sort Persons by Name and Age/1.Sort Persons by Name and Age/Team.cs
@@ -0,0 +1,45 @@
+﻿using PersonsInfo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+
+namespace _1.Sort_Persons_by_Name_and_Age
+{
+    public class Team
+    {
+		public Team(string name)
+		{
+			Name = name;
+			firstTeam = new List<Person>();
+			reserveTeam = new List<Person>();
+		}
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private List<Person> firstTeam;
+
+        public IReadOnlyCollection<Person> FirstTeam => firstTeam.AsReadOnly();
+       
+        private List<Person> reserveTeam;
+
+        public IReadOnlyCollection<Person> ReserveTeam	=> reserveTeam.AsReadOnly();
+
+		public void AddPlayer(Person person)
+		{
+			if (person.Age < 40)
+			{
+				firstTeam.Add(person);
+			}
+			else
+			{
+				reserveTeam.Add(person);
+			}
+		} 
+    }
+}
