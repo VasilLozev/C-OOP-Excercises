ac918849a93dd5dd8395fdec005b10abcaf672b9
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-01-08 18:41:46 +0200

    Add files via upload

---
+++ C# OOP Lab Excercises/Interfaces and Abstraction/02.Cars/Tesla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} {this.GetType().Name} {Model} with {Battery} Batteries");
+            sb.AppendLine($"{this.Start()}");
+            sb.AppendLine($"{this.Stop()}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
