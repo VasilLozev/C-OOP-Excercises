08f7aab57275e7863e8a98d9fd38e8211eda27d8
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-01-10 15:20:21 +0200

    Add files via upload

---
+++ Exercise Interfaces and Abstraction/Telephony/IO/FileWriter.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Telephony.IO
+{
+    using Interfaces;
+
+    public class FileWriter : IWriter
+    {
+        public void Write(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteLine(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
