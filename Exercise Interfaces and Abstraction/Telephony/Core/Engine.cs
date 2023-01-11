cd26b9485d624ce33546bd8c7698c917146d26b3
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-01-11 14:50:44 +0200

    Add files via upload

---
+++ Exercise Interfaces and Abstraction/Telephony/Core/Engine.cs
@@ -0,0 +1,82 @@
+﻿namespace Telephony.Core
+{
+    using System;
+
+    using Interfaces;
+    using Telephony.Exceptions;
+    using Telephony.IO.Interfaces;
+    using Telephony.Models;
+    using Telephony.Models.Interfaces;
+
+    public class Engine : IEngine
+    {
+        private readonly IReader reader;
+        private readonly IWriter writer;
+
+        private readonly IStationaryPhone stationaryPhone;
+        private readonly ISmartphone smartphone;
+
+        public Engine()
+        {
+            this.stationaryPhone = new StationaryPhone();
+            this.smartphone = new Smartphone();
+        }
+        public Engine(IReader reader, IWriter writer)
+            : this()
+        {
+            this.reader = reader;
+            this.writer = writer;
+        }
+
+        public void Run()
+        {
+            
+                string[] phoneNumbers = this.reader.ReadLine()
+                        .Split(' ');
+                string[] urls = this.reader.ReadLine()
+                        .Split(' ');
+
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                try
+                {
+                    if (phoneNumber.Length == 10)
+                    {
+                        this.writer.WriteLine(this.smartphone.Call(phoneNumber));
+                    }
+                    else if (phoneNumber.Length == 7)
+                    {
+                        this.writer.WriteLine(this.stationaryPhone.Call(phoneNumber));
+                    }
+                    else
+                    {
+                        throw new InvalidPhoneNumberException();
+                    }
+                }
+                catch (InvalidPhoneNumberException ipne)
+                {
+                    this.writer.WriteLine(ipne.Message);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            foreach (string url in urls)
+            {
+                try
+                {
+                    this.writer.WriteLine(this.smartphone.BrowseURL(url));
+                }
+                catch (InvalidURLException iue)
+                {
+                    this.writer.WriteLine(iue.Message);
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ Exercise Interfaces and Abstraction/Telephony/IO/FileWriter.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Telephony.IO
+{
+    using Interfaces;
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class FileWriter : IWriter
+    {
+        private string filePath;
+        public FileWriter(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public string FilePath
+        {
+            get
+            {
+                return this.filePath;
+            }
+            private set
+            {
+                if (File.Exists(value))
+                {
+                    throw new ArgumentException("Invalid file path!");
+                }
+                this.filePath = value;
+            }
+        }
+        public void Write(string text)
+        {
+            File.WriteAllText(filePath, text);
+        }
+
+        public void WriteLine(string text)
+        {
+            File.WriteAllText(filePath
+            ,$"{text} {Environment.NewLine}");
+        }
+    }
+}
