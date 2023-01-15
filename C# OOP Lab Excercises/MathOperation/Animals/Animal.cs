d0a2dd76c4d4d0930c876cacf02c670c451c71ff
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-01-16 01:49:03 +0200

    Add files via upload

---
+++ C# OOP Lab Excercises/MathOperation/Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+           Name = name;
+           FavouriteFood = favouriteFood;
+        }
+        public string Name;
+        public string FavouriteFood;
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood}";
+        }
+    }
+}
